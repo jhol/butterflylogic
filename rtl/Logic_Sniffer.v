7e8779ae5b987398742717a549e16a29130335d3
Iztok Jeras <iztok.jeras@gmail.com>
2013-04-28 07:52:33 +0200

    Verilog: top level module cleanup

--- rtl/Logic_Sniffer.v
+++ rtl/Logic_Sniffer.v
@@ -38,62 +38,48 @@
 
 `define COMM_TYPE_SPI 1		// comment out for UART mode
 
-module Logic_Sniffer(
-  bf_clock,
-  extClockIn,
-  extClockOut,
-  extTriggerIn,
-  extTriggerOut,
-  indata,
-`ifdef COMM_TYPE_SPI
-  miso, mosi, sclk, cs,
-`else
-  rx, tx,
-`endif
-  dataReady,
-  armLEDnn,
-  triggerLEDnn);
-
-parameter [31:0] MEMORY_DEPTH=6;
-parameter [31:0] CLOCK_SPEED=50;
-parameter [1:0] SPEED=2'b00;
-
+module Logic_Sniffer #(
+`ifndef COMM_TYPE_SPI
+  parameter FREQ = 100000000,       // limited to 100M by onboard SRAM
+  parameter TRXSCALE = 28,          // 100M / 28 / 115200 = 31 (5bit)  --If serial communications are not working then try adjusting this number.
+  parameter RATE = 115200,          // maximum & base rate
 // Sets the speed for UART communications
 // SYSTEM_JITTER = "1000 ps"
-input bf_clock;
-input extClockIn;
-output extClockOut;
-input extTriggerIn;
-output extTriggerOut;
-inout [31:0] indata;
-output dataReady;
-output armLEDnn;
-output triggerLEDnn;
-
-`ifdef COMM_TYPE_SPI
-  output miso;
-  input mosi;
-  input sclk;
-  input cs;
-`else
-  input rx;
-  output tx;
 `endif
-
-
-parameter FREQ = 100000000;  // limited to 100M by onboard SRAM
-parameter TRXSCALE = 28;  // 100M / 28 / 115200 = 31 (5bit)  --If serial communications are not working then try adjusting this number.
-parameter RATE = 115200;  // maximum & base rate
+  parameter [31:0] MEMORY_DEPTH=6,
+  parameter [31:0] CLOCK_SPEED=50,
+  parameter  [1:0] SPEED=2'b00
+)(
+  input  wire        bf_clock,
+  input  wire        extClockIn,
+  output wire        extClockOut,
+  input  wire        extTriggerIn,
+  output wire        extTriggerOut,
+  inout  wire [31:0] indata,
+  output wire        dataReady,
+  output wire        armLEDnn,
+  output wire        triggerLEDnn,
+`ifdef COMM_TYPE_SPI
+  output wire        miso,
+  input  wire        mosi,
+  input  wire        sclk,
+  input  wire        cs
+`else
+  input  wire        rx,
+  output wire        tx
+`endif
+);
 
 wire extReset = 1'b0;
 wire [39:0] cmd;
 wire [31:0] sram_wrdata;
 wire [35:0] sram_rddata; 
-wire [3:0] sram_rdvalid;
+wire  [3:0] sram_rdvalid;
 wire [31:0] stableInput;
 
-wire [7:0] opcode;
+wire  [7:0] opcode;
 wire [31:0] config_data; 
+
 assign {config_data,opcode} = cmd;
 
 
@@ -113,45 +99,14 @@ ddr_clkout extclock_pad (.pad(extClockOut), .clk(extclock));
 //
 // Configure the probe pins...
 //
-reg [10:0] test_counter, next_test_counter;
-always @ (posedge clock) 
-begin
-  test_counter = next_test_counter;
-end
-always @*
-begin
-  #1;
-  next_test_counter = test_counter+1'b1;
-end
+reg [10:0] test_counter;
+always @ (posedge clock, posedge extReset) 
+if (extReset) test_counter <= 'b0;
+else          test_counter <= test_counter + 'b1;
 
-wire [15:0] test_pattern = {
-    test_counter[10], test_counter[4],
-    test_counter[10], test_counter[4],
-    test_counter[10], test_counter[4],
-    test_counter[10], test_counter[4],
-    test_counter[10], test_counter[4],
-    test_counter[10], test_counter[4],
-    test_counter[10], test_counter[4],
-    test_counter[10], test_counter[4]};
+wire [15:0] test_pattern = {8{test_counter[10], test_counter[4]}};
 
-outbuf io_indata31 (.pad(indata[31]), .clk(clock), .outsig(test_pattern[15]), .oe(extTestMode));
-outbuf io_indata30 (.pad(indata[30]), .clk(clock), .outsig(test_pattern[14]), .oe(extTestMode));
-outbuf io_indata29 (.pad(indata[29]), .clk(clock), .outsig(test_pattern[13]), .oe(extTestMode));
-outbuf io_indata28 (.pad(indata[28]), .clk(clock), .outsig(test_pattern[12]), .oe(extTestMode));
-outbuf io_indata27 (.pad(indata[27]), .clk(clock), .outsig(test_pattern[11]), .oe(extTestMode));
-outbuf io_indata26 (.pad(indata[26]), .clk(clock), .outsig(test_pattern[10]), .oe(extTestMode));
-outbuf io_indata25 (.pad(indata[25]), .clk(clock), .outsig(test_pattern[9]), .oe(extTestMode));
-outbuf io_indata24 (.pad(indata[24]), .clk(clock), .outsig(test_pattern[8]), .oe(extTestMode));
-outbuf io_indata23 (.pad(indata[23]), .clk(clock), .outsig(test_pattern[7]), .oe(extTestMode));
-outbuf io_indata22 (.pad(indata[22]), .clk(clock), .outsig(test_pattern[6]), .oe(extTestMode));
-outbuf io_indata21 (.pad(indata[21]), .clk(clock), .outsig(test_pattern[5]), .oe(extTestMode));
-outbuf io_indata20 (.pad(indata[20]), .clk(clock), .outsig(test_pattern[4]), .oe(extTestMode));
-outbuf io_indata19 (.pad(indata[19]), .clk(clock), .outsig(test_pattern[3]), .oe(extTestMode));
-outbuf io_indata18 (.pad(indata[18]), .clk(clock), .outsig(test_pattern[2]), .oe(extTestMode));
-outbuf io_indata17 (.pad(indata[17]), .clk(clock), .outsig(test_pattern[1]), .oe(extTestMode));
-outbuf io_indata16 (.pad(indata[16]), .clk(clock), .outsig(test_pattern[0]), .oe(extTestMode));
-
-
+outbuf io_indata [31:16] (.pad(indata[31:16]), .clk(clock), .outsig(test_pattern[15:0]), .oe(extTestMode));
 
 //
 // Instantiate serial interface....
@@ -159,32 +114,40 @@ outbuf io_indata16 (.pad(indata[16]), .clk(clock), .outsig(test_pattern[0]), .oe
 `ifdef COMM_TYPE_SPI
 
 spi_slave spi_slave (
-  .clock(clock), 
-  .extReset(extReset),
-  .sclk(sclk), 
-  .cs(cs), 
-  .mosi(mosi),
-  .dataIn(stableInput),
-  .send(send), 
-  .send_data(sram_rddata[31:0]), 
-  .send_valid(sram_rdvalid),
+  .clock      (clock), 
+  .extReset   (extReset),
+  .sclk       (sclk), 
+  .cs         (cs),
+  .mosi       (mosi),
+  .dataIn     (stableInput),
+  .send       (send), 
+  .send_data  (sram_rddata[31:0]), 
+  .send_valid (sram_rdvalid),
   // outputs...
-  .cmd(cmd), .execute(execute), 
-  .busy(busy), .miso(miso));
+  .cmd        (cmd),
+  .execute    (execute), 
+  .busy       (busy),
+  .miso       (miso)
+);
 
 `else 
 
-eia232 #(.FREQ(FREQ), .SCALE(TRXSCALE), .RATE(RATE)) eia232 (
-  .clock(clock),
-  .reset(extReset),
-  .speed(SPEED),
-  .rx(rx),
-  .tx(tx),
-  .cmd(cmd),
-  .execute(execute),
-  .data(output),
-  .send(send),
-  .busy(busy));
+eia232 #(
+  .FREQ     (FREQ),
+  .SCALE    (TRXSCALE),
+  .RATE     (RATE)
+) eia232 (
+  .clock    (clock),
+  .reset    (extReset),
+  .speed    (SPEED),
+  .rx       (rx),
+  .tx       (tx),
+  .cmd      (cmd),
+  .execute  (execute),
+  .data     (output),
+  .send     (send),
+  .busy     (busy)
+);
 
 `endif 
 
@@ -192,44 +155,49 @@ eia232 #(.FREQ(FREQ), .SCALE(TRXSCALE), .RATE(RATE)) eia232 (
 //
 // Instantiate core...
 //
-core #(.MEMORY_DEPTH(MEMORY_DEPTH)) core (
-  .clock(clock),
-  .extReset(extReset),
-  .extClock(extClockIn),
-  .extTriggerIn(extTriggerIn),
-  .opcode(opcode),
-  .config_data(config_data),
-  .execute(execute),
-  .indata(indata),
-  .outputBusy(busy),
+core #(
+  .MEMORY_DEPTH    (MEMORY_DEPTH)
+) core (
+  .clock           (clock),
+  .extReset        (extReset),
+  .extClock        (extClockIn),
+  .extTriggerIn    (extTriggerIn),
+  .opcode          (opcode),
+  .config_data     (config_data),
+  .execute         (execute),
+  .indata          (indata),
+  .outputBusy      (busy),
   // outputs...
-  .sampleReady50(),
-  .stableInput(stableInput),
-  .outputSend(send),
-  .memoryWrData(sram_wrdata),
-  .memoryRead(read),
-  .memoryWrite(write),
-  .memoryLastWrite(lastwrite),
-  .extTriggerOut(extTriggerOut),
-  .extClockOut(extclock), 
-  .armLEDnn(armLEDnn),
-  .triggerLEDnn(triggerLEDnn),
-  .wrFlags(wrFlags),
-  .extTestMode(extTestMode));
-
+  .sampleReady50   (),
+  .stableInput     (stableInput),
+  .outputSend      (send),
+  .memoryWrData    (sram_wrdata),
+  .memoryRead      (read),
+  .memoryWrite     (write),
+  .memoryLastWrite (lastwrite),
+  .extTriggerOut   (extTriggerOut),
+  .extClockOut     (extclock), 
+  .armLEDnn        (armLEDnn),
+  .triggerLEDnn    (triggerLEDnn),
+  .wrFlags         (wrFlags),
+  .extTestMode     (extTestMode)
+);
 
 //
-// Instantiate the memory...
+// Instantiate the memory interface...
 //
 sram_interface sram_interface (
-  .clk(clock),
-  .wrFlags(wrFlags), 
-  .config_data(config_data[5:2]),
-  .write(write), .lastwrite(lastwrite), .read(read),
-  .wrdata({4'h0,sram_wrdata}),
+  .clk          (clock),
+  .wrFlags      (wrFlags), 
+  .config_data  (config_data[5:2]),
+  .write        (write),
+  .lastwrite    (lastwrite),
+  .read         (read),
+  .wrdata       ({4'h0,sram_wrdata}),
   // outputs...
-  .rddata(sram_rddata),
-  .rdvalid(sram_rdvalid));
+  .rddata       (sram_rddata),
+  .rdvalid      (sram_rdvalid)
+);
 
 endmodule
 
